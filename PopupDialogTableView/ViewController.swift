82c52fd095a519856be6dd31d9d029d772db7322
Martin Wildfeuer <mwfire@mwfire.de>
2016-12-31 16:06:19 +0100

    Initial Commit

---
+++ PopupDialogTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopupDialogTableView
+//
+//  Created by Martin Wildfeuer on 31.12.16.
+//  Copyright © 2016 mwfire development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
